0432ff6f917f4a39d784bb738d81380bbc0ec45e
Mark de Bruijn <mrdebruijn@gmail.com>
2016-01-22 15:46:55 +0100

    Properly daemonize log to syslog

--- smtpd-dual-delivery.py
+++ smtpd-dual-delivery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # SMTP dual delivery relay proxy daemon
-# Warning no queueing implemented mail might get lost if one of the receiving servers is not handling the mail correctly.
-# No Warranty whatsoever
-# (c) 2014 bkram
+# (c) 2016 bkram, GPL see LICENSE file
 
 import asyncore
 import ConfigParser
@@ -10,30 +8,36 @@ import email
 import smtpd
 import smtplib
 import time
-    
+import daemon
+import syslog
+
+
 def getconfig():
-    Config=ConfigParser.ConfigParser()
+    Config = ConfigParser.ConfigParser()
     try:
         Config.read('smtpd-dual-delivery.cfg')
-        bindip=Config.get('daemon','bindip')
-        bindport=Config.get('daemon','bindport')
-        destip_a=Config.get('destination_a','ip')
-        destport_a=Config.get('destination_a','port')
-        destip_b=Config.get('destination_b','ip')
-        destport_b=Config.get('destination_b','port')
-        return(bindip, bindport, destip_a, destport_a, destip_b, destport_b)
+        bindip = Config.get('daemon', 'bindip')
+        bindport = Config.get('daemon', 'bindport')
+        destip_a = Config.get('destination_a', 'ip')
+        destport_a = Config.get('destination_a', 'port')
+        destip_b = Config.get('destination_b', 'ip')
+        destport_b = Config.get('destination_b', 'port')
+        return (bindip, bindport, destip_a, destport_a, destip_b, destport_b)
     except:
         exit('Configuration error, please check smtpd-dual-delivery.cfg')
 
+
 def logtime():
     return time.strftime('%D %H:%M:%S')
+
 
 def getmesid(data):
     mailbody = email.message_from_string(data)
     return mailbody['Message-Id']
 
-def deliverto(ip, port, mailfrom, rcpttos, data,messageid):
-    print '%s %s Relay mail to %s:%s' % (logtime(), messageid, ip, port)
+
+def deliverto(ip, port, mailfrom, rcpttos, data, messageid):
+    syslog.syslog('%s %s Relay mail to %s:%s' % (logtime(), messageid, ip, port))
     try:
         sendserver = smtplib.SMTP(ip, port)
     except:
@@ -48,21 +52,27 @@ def deliverto(ip, port, mailfrom, rcpttos, data,messageid):
         return "Error: could not close connection to server %s:%s" % (ip, port)
     return 'Message relayed to %s:%s' % (ip, port)
 
+
 class CustomSMTPServer(smtpd.SMTPServer):
     def process_message(self, peer, mailfrom, rcpttos, data):
-        messageid=getmesid(data)
-        print '%s %s Start Proxy action' % (logtime(), messageid)
-        print '%s %s Message from %s to %s size %s via %s' % (logtime(), messageid, mailfrom, rcpttos, len(data), peer)
+        messageid = getmesid(data)
+        syslog.syslog('%s %s Start Proxy action' % (logtime(), messageid))
+        syslog.syslog(
+                '%s %s Message from %s to %s size %s via %s' % (
+                    logtime(), messageid, mailfrom, rcpttos, len(data), peer))
         state_a = deliverto(destip_a, destport_a, mailfrom, rcpttos, data, messageid)
-        print '%s %s %s' % (logtime(), messageid, state_a)
+        syslog.syslog('%s %s %s' % (logtime(), messageid, state_a))
         state_b = deliverto(destip_b, destport_b, mailfrom, rcpttos, data, messageid)
-        print '%s %s %s' % (logtime(), messageid, state_b)
-        print '%s %s End Proxy action' % (logtime(), messageid)
+        syslog.syslog('%s %s %s' % (logtime(), messageid, state_b))
+        syslog.syslog('%s %s End Proxy action' % (logtime(), messageid))
         return
 
+
 def main():
-    server = CustomSMTPServer((bindip, int(bindport)), None)
-    asyncore.loop()
+    with daemon.DaemonContext():
+        server = CustomSMTPServer((bindip, int(bindport)), None)
+        asyncore.loop()
+
 
 if __name__ == "__main__":
     (bindip, bindport, destip_a, destport_a, destip_b, destport_b) = getconfig()
--- smtpd-snoop-delivery.py
+++ smtpd-snoop-delivery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # SMTP snooping relay proxy daemon
-# Warning no queueing implemented mail might get lost if one of the receiving servers is not handling the mail correctly.
-# No Warranty whatsoever
-# (c) 2014 bkraM
+# (c) 2016 bkram, GPL see LICENSE file
 
 import asyncore
 import ConfigParser
@@ -11,32 +9,39 @@ import smtpd
 import smtplib
 import time
 import uuid
-    
+import daemon
+import syslog
+
+
 def getconfig():
-    Config=ConfigParser.ConfigParser()
+    Config = ConfigParser.ConfigParser()
     try:
         Config.read('smtpd-snoop-delivery.cfg')
-        bindip=Config.get('daemon','bindip')
-        bindport=Config.get('daemon','bindport')
-        destip=Config.get('destination','ip')
-        destport=Config.get('destination','port')
-        path=Config.get('file','path')
-        return(bindip, bindport, destip, destport, path)
+        bindip = Config.get('daemon', 'bindip')
+        bindport = Config.get('daemon', 'bindport')
+        destip = Config.get('destination', 'ip')
+        destport = Config.get('destination', 'port')
+        path = Config.get('file', 'path')
+        return (bindip, bindport, destip, destport, path)
     except:
         exit('Configuration error, please check smtpd-snoop-delivery.cfg')
+
 
 def logtime():
     return time.strftime('%D %H:%M:%S')
 
+
 def ftime():
     return time.strftime('%H-%M-%S')
+
 
 def getmesid(data):
     mailbody = email.message_from_string(data)
     return mailbody['Message-Id']
 
-def deliverto(ip, port, mailfrom, rcpttos, data,messageid):
-    print '%s %s Relay mail to %s:%s' % (logtime(), messageid, ip, port)
+
+def deliverto(ip, port, mailfrom, rcpttos, data, messageid):
+    syslog.syslog('%s %s Relay mail to %s:%s' % (logtime(), messageid, ip, port))
     try:
         sendserver = smtplib.SMTP(ip, port)
     except:
@@ -51,8 +56,9 @@ def deliverto(ip, port, mailfrom, rcpttos, data,messageid):
         return "Error: could not close connection to server %s:%s" % (ip, port)
     return 'Message relayed to %s:%s' % (ip, port)
 
-def savetofile(mailfrom, rcpttos, data,messageid):
-    fname = "%s%s-%s.eml" % (path,ftime(),str(uuid.uuid4()))
+
+def savetofile(mailfrom, rcpttos, data, messageid):
+    fname = "%s%s-%s.eml" % (path, ftime(), str(uuid.uuid4()))
     try:
         f = open(fname, 'w+')
         f.write(data)
@@ -61,21 +67,27 @@ def savetofile(mailfrom, rcpttos, data,messageid):
     except:
         return 'Error: Message could not be saved to : %s' % (fname)
 
+
 class CustomSMTPServer(smtpd.SMTPServer):
     def process_message(self, peer, mailfrom, rcpttos, data):
-        messageid=getmesid(data)
-        print '%s %s Start Proxy action' % (logtime(), messageid)
-        print '%s %s Message from %s to %s size %s via %s' % (logtime(), messageid, mailfrom, rcpttos, len(data), peer)
+        messageid = getmesid(data)
+        syslog.syslog('%s %s Start Proxy action' % (logtime(), messageid))
+        syslog.syslog(
+                '%s %s Message from %s to %s size %s via %s' % (
+                logtime(), messageid, mailfrom, rcpttos, len(data), peer))
         state = deliverto(destip, destport, mailfrom, rcpttos, data, messageid)
-        print '%s %s %s' % (logtime(), messageid, state)
+        syslog.syslog('%s %s %s' % (logtime(), messageid, state))
         state = savetofile(mailfrom, rcpttos, data, messageid)
-        print '%s %s %s' % (logtime(), messageid, state)
-        print '%s %s End Proxy action' % (logtime(), messageid)
+        syslog.syslog('%s %s %s' % (logtime(), messageid, state))
+        syslog.syslog('%s %s End Proxy action' % (logtime(), messageid))
         return
 
+
 def main():
-    server = CustomSMTPServer((bindip, int(bindport)), None)
-    asyncore.loop()
+    with daemon.DaemonContext():
+        server = CustomSMTPServer((bindip, int(bindport)), None)
+        asyncore.loop()
+
 
 if __name__ == "__main__":
     (bindip, bindport, destip, destport, path) = getconfig()
